c14f322641a473f35769bca5e97ca909b024f0f1
devprajwolgotnochill <devprajwolgotnochill@gmail.com>
2026-04-05 23:06:45 +0545

    complet project 1 bg-change

--- BG-changer/src/App.jsx
+++ BG-changer/src/App.jsx
@@ -1,121 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bgColor, setBgColor] = useState('bg-black');
+
+  // function changeColor(color) {
+  //   setBgColor(color);
+  // }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    
 
-      <div className="ticks"></div>
+    <div className={`w-full h-screen flex duration-200 ${bgColor}`}>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      <div className='flex flex-wrap fixed justify-center top-0 inset-x-0 px-2'>
+        <button className='w-20 h-20 m-2 rounded-full bg-red-500 border-2 border-white' onClick={() => setBgColor('bg-red-500') }>RED</button>
+        
+        <button className='w-20 h-20 m-2 rounded-full bg-green-500 border-2 border-white' onClick={() => setBgColor('bg-green-500') }>GREEN</button>
+
+        <button className='w-20 h-20 m-2 rounded-full bg-blue-500 border-2 border-white' onClick={() => setBgColor('bg-blue-500') }>BLUE</button>
+
+        <button className='w-20 h-20 m-2 rounded-full bg-yellow-500 border-2 border-white' onClick={() => setBgColor('bg-yellow-500') }>YELLOW</button>
+
+        <button className='w-20 h-20 m-2 rounded-full bg-purple-500 border-2 border-white' onClick={() => setBgColor('bg-purple-500') }>PURPLE</button>
+      </div>
+      
+
+
+    </div>
+  ) 
 }
 
 export default App
